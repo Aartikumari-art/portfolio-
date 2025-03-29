a65e46bf11fa6b722a1c6ccefeda08c6c0cbf802
Aarti Kumari <kaarti.kc@gmail.com>
2025-03-29 14:12:21 +0530

    profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import { animate, motion } from "framer-motion";
 import TypeWriter from "typewriter-effect";
 import { BsArrowUpRight , BsChevronDown } from "react-icons/bs";
-import me from "../assets/tree.jpeg";
+import me from "../assets/portfolio.jpeg";
 
 
 export const Home = () => {
